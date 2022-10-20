8bb115bcbd56399c312e8a28534cf559539b40a7
Milsevka <lusindami99@gmail.com>
2022-10-21 01:43:22 +0300

    feat: add local storage

--- gem-puzzle/js/index.js
+++ gem-puzzle/js/index.js
@@ -3,13 +3,22 @@ container.className = "main-container";
 container.innerHTML = "It's a Gem Puzzle. Good luck!";
 document.body.append(container);
 
-let popap = document.createElement("div");
-popap.className = "popap";
-container.append(popap);
+let popup = document.createElement("div");
+popup.className = "popup";
+container.append(popup);
 
-let popapContetn = document.createElement("div");
-popapContetn.className = "popap-content";
-popap.append(popapContetn);
+let popupContetn = document.createElement("div");
+popupContetn.className = "popup-content";
+popup.append(popupContetn);
+
+let popupResults = document.createElement("div");
+popupResults.className = "popup-results";
+container.append(popupResults);
+
+let resultsCont = document.createElement("div");
+resultsCont.className = "results-container";
+resultsCont.innerHTML = "TOP-10"
+popupResults.append(resultsCont);
 
 let options = document.createElement("div");
 options.className = "options";
@@ -70,48 +79,53 @@ container.append(sizeText);
 
 let sizeCounter = document.createElement("div");
 sizeCounter.className = "size-counter";
-sizeCounter.innerHTML = "4*4";
+sizeCounter.innerHTML = "4x4";
  sizeText.append(sizeCounter);
 
 let sizeContainer = document.createElement("div");
 sizeContainer.className = "size-container";
+sizeContainer.innerHTML = "Other size:"
 container.append(sizeContainer);
 
 let sizethree = document.createElement("div");
 sizethree.className = "size-three";
 sizethree.id = 3
-sizethree.innerHTML = "3*3";
+sizethree.innerHTML = "3x3";
 sizeContainer.append(sizethree);
 
 let sizefour = document.createElement("div");
 sizefour.className = "size-four";
 sizefour.id = 4
-sizefour.innerHTML = "4*4";
+sizefour.innerHTML = "4x4";
 sizeContainer.append(sizefour);
 
 let sizefive = document.createElement("div");
 sizefive.className = "size-five";
 sizefive.id = 5
-sizefive.innerHTML = "5*5";
+sizefive.innerHTML = "5x5";
 sizeContainer.append(sizefive);
 
 let sizesix = document.createElement("div");
 sizesix.className = "size-six";
-sizesix.innerHTML = "6*6";
+sizesix.innerHTML = "6x6";
 sizesix.id = 6
 sizeContainer.append(sizesix);
 
 let sizeseven = document.createElement("div");
 sizeseven.className = "size-seven";
-sizeseven.innerHTML = "7*7";
+sizeseven.innerHTML = "7x7";
 sizeseven.id = 7
 sizeContainer.append(sizeseven);
 
 let sizeeight = document.createElement("div");
 sizeeight.className = "size-eight";
-sizeeight.innerHTML = "8*8";
+sizeeight.innerHTML = "8x8";
 sizeeight.id = 8
 sizeContainer.append(sizeeight);
+
+let ul = document.createElement("ul");
+ul.className = "res";
+resultsCont.append(ul);
 
 let amount = 15;
 let rows = 4; 
@@ -123,7 +137,6 @@ sizeContainer.onclick = function (event) {
   target.className === 'size-six' || target.className === 'size-seven' || target.className === 'size-eight') {
     rows = target.id
     size = target.innerHTML
-  console.log(target.className)
     amount = Number(target.id * target.id - 1) 
     pazzleContainer.innerHTML = ''
        cellsArray.length = 0;
@@ -146,11 +159,12 @@ const freeCard = {
 
 let cellsArray;
 let counterWin = 0;
+let counterTop = 0;
 
 
 function change(index) {
   let widthCard = 320 / rows
- 
+  
   
   const card = cellsArray[index];
   const diffPositionRight = Math.abs(freeCard.left - card.left);
@@ -161,38 +175,39 @@ function change(index) {
 
   card.element.style.top = `${freeCard.top * widthCard}px`;
   card.element.style.left = `${freeCard.left * widthCard}px`;
-
+  movesCounter.innerHTML++
   const emtRight = freeCard.left;
   const emtBottom = freeCard.top;
   freeCard.left = card.left;
   freeCard.top = card.top;
   card.left = emtRight;
   card.top = emtBottom;
-  console.log(cellsArray)
+ 
   const isFinished = cellsArray.every((card) => {
-    // console.log(card.bottom * rows + card.right)
-    // console.log(card.value)
     return card.value === card.top * rows + card.left + 1;
   });
 
   if (isFinished) {
     clearTimer()
-    popap.classList.add('active')
-    popapContetn.innerHTML = `You won! You solved the puzzle in ${movesCounter.innerHTML} steps and in ${timeCounter.innerHTML} sec!`
+    counterTop++
+    let stringLocal = `Moves: ${movesCounter.innerHTML} - Time: ${timeCounter.innerHTML}`
+    ul.append(...getListContent());
+    popup.classList.add('active')
+    popupContetn.innerHTML = `You won! You solved the puzzle in ${movesCounter.innerHTML} steps and in ${timeCounter.innerHTML} sec!`
+     localStorage.setItem('moves', stringLocal)
+     function getListContent() {
+      let result = [];
+      for(let i=1; i<=1; i++) {
+        let li = document.createElement('li');
+        li.append(`${counterTop} - ${localStorage.getItem('moves')}`);
+        result.push(li);
+      }
+      return result;
+    }
   }
 
 }
 
-// function setLocalStorage() {
-//   localStorage.setItem('result', nameMain.value);
-// }
-
-
-// function getLocalStorage() {
-//   if(localStorage.getItem('result')) {
-//     nameMain.value = localStorage.getItem('result');
-//   }
-// }
 
 
 function prepTable() {
@@ -203,10 +218,9 @@ cellsArray = [];
 cellsArray.push(freeCard);
 let widthCard = 320 / rows
 const numbers = [...Array(amount).keys()]
-// .reverse()
+
 .sort(() => Math.random() - 0.5);
-// console.log(numbers)
-// console.log(cellsArray)
+
 for (let i = 1; i <= amount; i++) {
     let pazzleCard = document.createElement("div");
     let value = numbers[i - 1] + 1;
@@ -230,7 +244,7 @@ for (let i = 1; i <= amount; i++) {
     pazzleContainer.append(pazzleCard);
     pazzleCard.addEventListener("click", () => {
       change(i);
-      movesCounter.innerHTML++
+      // movesCounter.innerHTML++
     });
   }
   
@@ -269,8 +283,19 @@ shuffle.addEventListener ("click" , () => {
   clearTimer()
   prepTable()
   startTimer()
-})
+});
 
-popap.addEventListener("click", () => {
-  popap.classList.remove('active')
-} )
+popup.addEventListener("click", () => {
+  popup.classList.remove('active')
+});
+
+results.addEventListener("click" , () => {
+  popupResults.classList.add('activep');
+});
+
+popupResults.addEventListener("click", () => {
+  popupResults.classList.remove('activep')
+});
+
+
+
